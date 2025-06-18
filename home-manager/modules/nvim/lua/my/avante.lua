fdc2e90e681b926ead2d98d89a8de64a30ae8e6d
Matthias Koenig <matthias.koenig@gmail.com>
2025-06-18 13:27:12 +0200

    flake update, avante ollama

--- home-manager/modules/nvim/lua/my/avante.lua
+++ home-manager/modules/nvim/lua/my/avante.lua
@@ -10,41 +10,62 @@ function M.setup()
 	})
 
 	require("avante").setup({
-
-		-- @alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
-		provider = "openrouter", -- The provider used in Aider mode or in the planning phase of Cursor Planning Mode
+		---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
+		---@type Provider
+		provider = "ollama", -- The provider used in Aider mode or in the planning phase of Cursor Planning Mode
+		---@alias Mode "agentic" | "legacy"
+		---@type Mode
+		mode = "agentic", -- The default mode for interaction. "agentic" uses tools to automatically generate code, "legacy" uses the old planning method to generate code.
 		-- WARNING: Since auto-suggestions are a high-frequency operation and therefore expensive,
 		-- currently designating it as `copilot` provider is dangerous because: https://github.com/yetone/avante.nvim/issues/1048
 		-- Of course, you can reduce the request frequency by increasing `suggestion.debounce`.
 		auto_suggestions_provider = "claude",
-		cursor_applying_provider = nil, -- The provider used in the applying phase of Cursor Planning Mode, defaults to nil, when nil uses Config.provider as the provider for the applying phase
-		openai = {
-			endpoint = "",
-			model = "qwen2.5-coder:32b",
-			temperature = 0,
-			max_tokens = 4096,
-		},
-		vendors = {
-			llmhub = {
-				__inherited_from = "openai",
-				disable_tools = true, -- { "python" },
-				endpoint = "https://api.llmhub.infs.ch/v1",
-				api_key_name = "LLMHUB_API_KEY",
-				model = "llama3.2:3b",
-				-- model = "qwq:32b",
+		providers = {
+			claude = {
+				endpoint = "https://api.anthropic.com",
+				model = "claude-3-5-sonnet-20241022",
+				extra_request_body = {
+					temperature = 0.75,
+					max_tokens = 4096,
+				},
+			},
+			ollama = {
+				endpoint = "http://152.96.151.56:11434", -- Note that there is no /v1 at the end.
+				model = "qwen3:1.7b",
 			},
 			openrouter = {
 				__inherited_from = "openai",
 				disable_tools = true, --{ "python" },
 				endpoint = "https://openrouter.ai/api/v1",
 				api_key_name = "OPENROUTER_API_KEY",
-				model = "google/gemma-3-12b-it:free",
+				--model = "google/gemma-3-12b-it:free",
+				model = "qwen/qwen3-8b:free",
+				extra_request_body = {
+					temperature = 0.75,
+					max_tokens = 4096,
+				},
+
 				--model = "mistralai/mistral-small-3.1-24b-instruct:free",
 				-- model = "mistralai/mistral-small-3.1-24b-instruct:free",
 				-- model = "meta-llama/llama-4-maverick:free",
 			},
 		},
-
+		---Specify the special dual_boost mode
+		---1. enabled: Whether to enable dual_boost mode. Default to false.
+		---2. first_provider: The first provider to generate response. Default to "openai".
+		---3. second_provider: The second provider to generate response. Default to "claude".
+		---4. prompt: The prompt to generate response based on the two reference outputs.
+		---5. timeout: Timeout in milliseconds. Default to 60000.
+		---How it works:
+		--- When dual_boost is enabled, avante will generate two responses from the first_provider and second_provider respectively. Then use the response from the first_provider as provider1_output and the response from the second_provider as provider2_output. Finally, avante will generate a response based on the prompt and the two reference outputs, with the default Provider as normal.
+		---Note: This is an experimental feature and may not work as expected.
+		dual_boost = {
+			enabled = false,
+			first_provider = "openai",
+			second_provider = "claude",
+			prompt = "Based on the two reference outputs below, generate a response that incorporates elements from both but reflects your own judgment and unique perspective. Do not provide any explanation, just give the response directly. Reference Output 1: [{{provider1_output}}], Reference Output 2: [{{provider2_output}}]",
+			timeout = 60000, -- Timeout in milliseconds
+		},
 		behaviour = {
 			auto_suggestions = false, -- Experimental stage
 			auto_set_highlight_group = true,
@@ -105,7 +126,7 @@ function M.setup()
 			---@type "right" | "left" | "top" | "bottom"
 			position = "right", -- the position of the sidebar
 			wrap = true, -- similar to vim.o.wrap
-			width = 30, -- default % based on available width
+			width = 40, -- default % based on available width
 			sidebar_header = {
 				enabled = true, -- true, false to enable/disable the header
 				align = "center", -- left, center, right for title
